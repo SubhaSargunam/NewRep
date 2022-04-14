bd394f04093f01515aa4ebb42443e5948199b8ca
SubhaSargunam <subhasargunam@gmail.com>
2022-04-14 11:35:12 +0530

    First Push

---
+++ src/test/java/org/sample/Sum.java
@@ -0,0 +1,20 @@
+package org.sample;
+
+public class Sum {
+
+public static void main(String[] args) {
+	addition();
+	}
+public static void addition() {
+	int a=10;
+	int b=20;
+	int c=a+b;
+	System.out.println(c);
+}
+	
+}
+
+
+
+
+
